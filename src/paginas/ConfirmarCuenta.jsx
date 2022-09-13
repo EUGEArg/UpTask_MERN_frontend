e4ca69aa818fc9365ae4f4faf594d2336a72da9c
EUGEarg <eugeniarivarossa@gmail.com>
2022-09-13 17:43:26 -0300

    confirmación de cuenta

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -5,20 +5,35 @@ import Alerta from '../components/Alerta'
 
 
 const ConfirmarCuenta = () => {
-	const params = useParams(); //Para leer los valores de la url
+	const [alerta, Setalerta] = useState({})
+	const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+
+	const params = useParams(); //Leer los valores de la url
 	const { id } = params //Extraer el id 
 
 	useEffect(() =>{
 		const ConfirmarCuenta = async () => { //Hace un llamado hacia la API
 			try{
 				const url = `http://localhost:4000/api/usuarios/confirmar/${id}`
-				const { data } = await axios.get
+				const { data } = await axios(url) //Tipo GET
+				
+				Setalerta({
+					msg: data.msg,
+					error: false
+				})
+				setCuentaConfirmada(true)
+
 			}catch(error){
-				console.log(error)
+				Setalerta({
+					msg:error.response.data.msg,
+					error: true
+				})
 			}
 		}
 		ConfirmarCuenta();
 	},[]) //Una sola vez se ejecuta por eso vacío
+
+	const { msg } = alerta
 
 	console.log(params)
 
@@ -26,7 +41,19 @@ const ConfirmarCuenta = () => {
 	return (
 		<>
 			<h1 className="text-sky-600 font-black text-5xl capitalize">Confirma tu Cuenta y Comienza a Crear Tus {''}<span className="text-slate-600">proyectos</span></h1>
+		
+			<div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white'>
+				{msg && <Alerta alerta={alerta}/>} 
+				{cuentaConfirmada && (
+					<Link
+						className="block text-center my-5 text-slate-500 text-sm uppercase"
+						to="/"
+					>Inicia Sesión </Link>
+				)}
+			</div>
+
 		</>
+		
 	)
 }
 
